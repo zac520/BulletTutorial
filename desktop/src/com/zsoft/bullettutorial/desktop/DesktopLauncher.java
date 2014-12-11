2022a4076e65612c8aa87dd0c637aa6fe2248af9
Zachary Knudson <zac520@aol.com>
2014-12-10 19:02:46 -0500

    Initial commit
    This has a cone as a player, and the player can only jump. Kind of.

---
+++ desktop/src/com/zsoft/bullettutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zsoft.bullettutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zsoft.bullettutorial.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
---
+++ html/src/com/zsoft/bullettutorial/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.zsoft.bullettutorial.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.zsoft.bullettutorial.MainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MainGame();
+        }
+}
